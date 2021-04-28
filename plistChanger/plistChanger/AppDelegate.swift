d8070c3a24e1ef4b0d7cf72ffe6131cb3f9ef3c7
jiaheng.song <jiaheng.song@me2zen.com>
2021-04-28 15:44:16 +0800

    点击关闭app

--- plistChanger/plistChanger/AppDelegate.swift
+++ plistChanger/plistChanger/AppDelegate.swift
@@ -11,14 +11,18 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     
-
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        
     }
 
 

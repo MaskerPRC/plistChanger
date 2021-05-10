a003d4b7d281d0357c677b5763433d1bc61f8b40
jiaheng.song <jiaheng.song@me2zen.com>
2021-05-10 15:16:56 +0800

    1

---
+++ 1/1/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  1
+//
+//  Created by 宋佳恒 on 2021/5/8.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}

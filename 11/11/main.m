9e028afe9b21ac7e25b7ffb3f89188030fc62a63
jiaheng.song <jiaheng.song@me2zen.com>
2021-06-04 14:17:00 +0800

    1

---
+++ 11/11/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  11
+//
+//  Created by 宋佳恒 on 2021/6/3.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AppleScriptObjC/AppleScriptObjC.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    [[NSBundle mainBundle] loadAppleScriptObjectiveCScripts];
+    return NSApplicationMain(argc, argv);
+}
